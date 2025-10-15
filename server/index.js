22d42ac58efad7b3a78f517a2c0eaf07ae3baa3d
Tooma81 <toomas.kaarel@gmail.com>
2025-10-15 14:55:09 +0300

    post and get comments

--- server/index.js
+++ server/index.js
@@ -22,6 +22,24 @@ app.post('/posts', (req, res) => {
 	});
 });
 
+const postComments = []
+
+app.get('/posts/:id/comments', (req, res) => {
+	res.json(postComments.filter(comment => comment.postId === req.params.id));
+})
+
+app.post('/posts/:id/comments', (req, res) => {
+	const postId = req.params.id;
+	const content = req.body.content;
+	const comment = {
+		id: randomBytes(4).toString('hex'),
+		postId,
+		content
+	};
+	postComments.push(comment);
+	res.status(201).json(comment);
+});
+
 app.listen(5000, () => {
 	console.log('Server is running on http://localhost:5000');
 })
